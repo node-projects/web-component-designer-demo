61329ceedb2b5ca903a9aafa932157bc2e100577
jogibear9988 <jochen.kuehner@gmx.de>
2022-03-22 22:48:32 +0000

    Deploying to gh-pages from @ node-projects/web-component-designer-demo@cefd24126d9ef8c1fa12e5ddafb7e0866f69dff9 🚀

--- dist/services/CustomBindableObjectDragDropService.js
+++ dist/services/CustomBindableObjectDragDropService.js
@@ -1,21 +1,59 @@
-import { DesignItem, InsertAction } from '/web-component-designer-demo/node_modules/@node-projects/web-component-designer/./dist/index.js';
+import { DesignItem, InsertAction, OverlayLayer } from '/web-component-designer-demo/node_modules/@node-projects/web-component-designer/./dist/index.js';
 export class CustomBindableObjectDragDropService {
-    dragEnter(designerCanvas, event) { }
-    dragLeave(designerCanvas, event) { }
-    dragOver(designerCanvas, event) {
+    constructor() {
+        this.rectMap = new Map();
+    }
+    dragEnter(designerCanvas, event) {
+        const element = designerCanvas.getElementAtPoint({ x: event.x, y: event.y });
+        const designItem = DesignItem.GetDesignItem(element);
+        console.log("enter", event, designItem);
+        if (designItem && !designItem.isRootItem) {
+            let itemRect = designerCanvas.getNormalizedElementCoordinates(element);
+            this.rect = designerCanvas.overlayLayer.drawRect(itemRect.x, itemRect.y, itemRect.width, itemRect.height, '', null, OverlayLayer.Background);
+            this.rect.style.fill = '#ff0000';
+            this.rect.style.opacity = '0.3';
+            this.rectMap.set(element, this.rect);
+        }
+    }
+    dragLeave(designerCanvas, event) {
+        const element = designerCanvas.getElementAtPoint({ x: event.x, y: event.y });
+        const designItem = DesignItem.GetDesignItem(element);
+        console.log("leave", event, designItem);
+        if (designItem && !designItem.isRootItem) {
+            const rect = this.rectMap.get(element);
+            designerCanvas.overlayLayer.removeOverlay(rect);
+            this.rectMap.delete(element);
+        }
+    }
+    dragOver(designerView, event) {
         return 'copy';
     }
     drop(designerCanvas, event, bindableObject) {
-        const position = designerCanvas.getNormalizedEventCoordinates(event);
-        const input = document.createElement('input');
-        const di = DesignItem.createDesignItemFromInstance(input, designerCanvas.serviceContainer, designerCanvas.instanceServiceContainer);
-        const grp = di.openGroup("Insert");
-        di.setAttribute('value', "[[this.objects['" + bindableObject.fullName + "']]]");
-        di.setStyle('position', 'absolute');
-        di.setStyle('left', position.x + 'px');
-        di.setStyle('top', position.y + 'px');
-        designerCanvas.instanceServiceContainer.undoService.execute(new InsertAction(designerCanvas.rootDesignItem, designerCanvas.rootDesignItem.childCount, di));
-        grp.commit();
-        requestAnimationFrame(() => designerCanvas.instanceServiceContainer.selectionService.setSelectedElements([di]));
+        for (let r of this.rectMap.values()) {
+            designerCanvas.overlayLayer.removeOverlay(r);
+        }
+        this.rectMap.clear();
+        const element = event.composedPath()[0];
+        const designItem = DesignItem.GetDesignItem(element);
+        if (designItem && !designItem.isRootItem) {
+            // Add binding to drop target...
+            if (element instanceof HTMLInputElement) {
+            }
+            else {
+            }
+        }
+        else {
+            const position = designerCanvas.getNormalizedEventCoordinates(event);
+            const input = document.createElement('input');
+            const di = DesignItem.createDesignItemFromInstance(input, designerCanvas.serviceContainer, designerCanvas.instanceServiceContainer);
+            const grp = di.openGroup("Insert");
+            di.setAttribute('value', "[[this.objects['" + bindableObject.fullName + "']]]");
+            di.setStyle('position', 'absolute');
+            di.setStyle('left', position.x + 'px');
+            di.setStyle('top', position.y + 'px');
+            designerCanvas.instanceServiceContainer.undoService.execute(new InsertAction(designerCanvas.rootDesignItem, designerCanvas.rootDesignItem.childCount, di));
+            grp.commit();
+            requestAnimationFrame(() => designerCanvas.instanceServiceContainer.selectionService.setSelectedElements([di]));
+        }
     }
 }
